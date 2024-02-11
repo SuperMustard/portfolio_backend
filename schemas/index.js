45b4278edba989793fb5a2b3ea3818c676289580
SuperMustard <mr.jiahanxin@gmail.com>
2024-02-11 17:03:47 -0500

    init commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import abouts from "./abouts"
+import works from "./works"
+import skills from "./skills"
+import experiences from "./experiences"
+import workExperience from "./workExperience"
+import contact from "./contact"
+
+export const schemaTypes = [
+    abouts,
+    works,
+    skills,
+    experiences,
+    workExperience,
+    contact
+]
